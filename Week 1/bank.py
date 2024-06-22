927ed2b50bed0d2719c9dd7127cbc06fe7086d74
Yessesity <izeaharquillano@gmail.com>
2024-06-22 20:18:29 +0800

    Update bank.py

--- Week 1/bank.py
+++ Week 1/bank.py
@@ -1,3 +1,13 @@
-test = input().strip().lower()
+def main ():
+    greeting = input ("Greeting: ")
+    greetinginator(greeting)
 
-print(test)
+def greetinginator(g):
+    if g.strip().lower().startswith("hello"):
+        print ("$0")
+    elif g.strip().lower().startswith("h"):
+        print ("$20")
+    else:
+        print ("$100")
+
+main()
