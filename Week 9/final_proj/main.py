fffede922fafcfbed1a4f0ec0fdab21a955334b9
Yessesity <izeaharquillano@gmail.com>
2024-07-17 19:57:13 +0800

    Update main.py

--- Week 9/final_proj/main.py
+++ Week 9/final_proj/main.py
@@ -1,8 +1,22 @@
-from char import Char
+from charwrite import CharWrite
+import csv
+
 
 def main():
-    char = Char.get_name()
-    char.write()
+    binary = CharWrite.has_char()
+    char = CharWrite.get_name(binary)
+    char.write(binary)
+    sheet = read(char)
+    print(sheet)
+    
+            
+def read(char):
+    sheet = []
+    with open(f"charsheet_{char}.csv") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            sheet.append(row)
+    return sheet
 
 
 if __name__ == "__main__":
