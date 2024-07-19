5d7c07e72611528c63bbc409996c83cefbbd1259
Yessesity <izeaharquillano@gmail.com>
2024-07-19 21:39:26 +0800

    Update project.py

--- Week 9/pomodoro/project.py
+++ Week 9/pomodoro/project.py
@@ -1,19 +1,65 @@
 import csv
 import sys
+import time
+from playsound import playsound
 
 
 def main():
     binary = get()
+    work, rest = get_work_rest(binary)
+    while True:
+        timer_interface(work, "\nTime to start working! Type 'start' to initialize the timer.")
+        playsound("alarm.mp3")
+        timer_interface(rest, "\nTime to rest! Type 'start' to initialize rest timer.")
+        playsound("alarm.mp3")
+        if not repeat():
+            return
+        
+
+def get_work_rest(binary):
     if binary == "n":
         work = 25
         rest = 5
+        return work, rest
     elif binary == "y":
         try:
             work, rest = get_durations()
             write_durations(work, rest)
         except TypeError:
             work, rest = reader()
-    print(f"Work Duration: {work}, Rest Duration {rest}")
+        return work, rest
+        
+
+def repeat():
+    while True:
+        binary = input("Do 1 more set of Pomodoro? (y/n)\n").lower().strip()
+        if binary == "y":
+            return True
+        elif binary == "n":
+            return False
+
+
+def timer_interface(time, text):
+    print("-" * 60, text)
+    while True:
+        start = input("").lower().strip()
+        if start != "start":
+            pass
+        else:
+            print("Starting timer...")
+            countdown(int(time)*60)
+            return
+
+
+def countdown(t): 
+    while t: 
+        mins, secs = divmod(t, 60) 
+        timer = "{:02d}:{:02d}".format(mins, secs) 
+        print(timer, end="\r") 
+        time.sleep(1) 
+        t -= 1
+    print("Times up!") 
+
 
 
 def get() -> str:
@@ -53,7 +99,7 @@ def get_durations() -> int:
             rest = int(
                 input("Input the duration (in minutes) of your desired rest:\n").strip()
             )
-            if work > 60 or rest > 60:
+            if work > 60 or rest > 60 or work <= 0 or rest <=0:
                 print("-" * 60, "\nInvalid input/s, try again")
                 pass
             else:
